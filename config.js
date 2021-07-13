133f5c956483e25d5d762618374545e30b4e4fbc
anhtrau2k5 <87364553+anhtrau2k5@users.noreply.github.com>
2021-07-13 16:33:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
     introTitle: 'Sannn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introDesc: `Anh sắp chuyển nhà rồi… Chuyển hộ khẩu vào trái tim em!`,
+    btnIntro: 'Hi Bae:3',
+    title: 'Bầu trời xanh, làn mây trắng. Em yêu nắng hay yêu anh? 😙',
+    desc: '#dangphamnguyetsang ',
+    btnYes: 'Yêu anh đó <33',
+    btnNo: 'Nắng nhaaa :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh deptrai vllll <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h chơi game nhé bạn.',
+    messDesc: 'Tối nay chơi game nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
 }
