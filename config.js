ac0c1c310e9e668dd38ed1a2019617c70076a8b0
Nguyễn Việt Ánh <87364553+anhtrau2k5@users.noreply.github.com>
2021-12-07 17:55:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Sannn à!',
+    introTitle: 'CON CHÓ THẢOOO!',
     introDesc: `Anh sắp chuyển nhà rồi… Chuyển hộ khẩu vào trái tim em!`,
     btnIntro: 'Hi Bae:3',
     title: 'Bầu trời xanh, làn mây trắng. Em yêu nắng hay yêu anh? 😙',
-    desc: '#dangphamnguyetsang ',
+    desc: '#tathithuthao ',
     btnYes: 'Yêu anh đó <33',
     btnNo: 'Nắng nhaaa :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Vì anh deptrai vllll <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay chơi game nhé công chúa.',
+    messDesc: 'Mai mua cho tớ cái bánh rán nhé !!!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/sss2005/posts/122928463338536'
 }
